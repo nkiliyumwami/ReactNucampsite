c4b8391e0a8726f47de617bb205dbba0b3269757
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-04-25 22:40:52 -0400

    Redux Thunk and Logger

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Card, CardBody, CardText, CardTitle, CardImg, BreadcrumbItem, Breadcrumb, Button, Modal, ModalBody, ModalHeader,Label} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import {Loading} from './LoadingComponent';
 
       function RenderCampsite({campsite}) {
           return(
@@ -39,6 +40,26 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
       }
 
     function CampsiteInfo(props) {
+        if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
         if(props.campsite) {
             return(
                 <div className="container">
