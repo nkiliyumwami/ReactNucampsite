a50400b4c1689e230be33102672d0367f2013bff
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-04-25 23:12:10 -0400

    React Redux Forms Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,6 +5,8 @@ import {Campsites} from './campsites';
 import {Comments} from './comments';
 import {Partners} from './partners';
 import {Promotions} from './promotions';
+import {createForms} from 'react-redux-form';
+import {InitialFeedback} from './forms';
 
 
 export const ConfigureStore = () => {
@@ -13,7 +15,10 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
